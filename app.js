f2602d23dc60cd7ec3b7942c775a37767cc70701
juanmarin444 <30785213+Juanmarin444@users.noreply.github.com>
2022-06-01 22:46:48 -0700

    Finished Game

--- app.js
+++ app.js
@@ -1,39 +1,69 @@
 let body = $("body");
-let wrong = false;
-let clicked = false;
-let transition = false;
+let btns = $(".btn");
+let title = $(".title");
+let colors = ['red', 'green', 'blue', 'yellow'];
 
-setTimeout(function () {
-    wrong = true;
+let answers = [];
+let userGuesses = [];
+let gameStarted = false;
+let level = 0;
+let gameOver = new Audio('sounds/gameover.mp3');
 
-    if (wrong) {
+document.addEventListener('keydown', () => {
+    if (!gameStarted) {
+        answers = [];
+        userGuesses = [];
+        level = 0;
+        addAnswer();
+        gameStarted = true;
+    }
+});
+
+$(".btn").click(function () {
+    let guess = $(this).attr("id");
+    userGuesses.push(guess);
+    let btnSound = new Audio(`sounds/${guess}.mp3`);
+    btnSound.play();
+    checkGuess();
+
+    $(this).addClass("click");
+    setTimeout(function () {
+        $(`#${guess}`).removeClass("click");
+    }, 131.25);
+
+});
+
+function addAnswer() {
+    userGuesses = [];
+    let rndBtnIdx = Math.floor(Math.random() * 4);
+    let newBtn = btns[rndBtnIdx];
+    answers.push(colors[rndBtnIdx]);
+    let transitionSound = new Audio(`sounds/${colors[rndBtnIdx]}.mp3`);
+    transitionSound.play();
+    newBtn.classList.add("transition");
+    setTimeout(function () {
+        newBtn.classList.remove("transition");
+    }, 162.5);
+}
+
+function checkGuess() {
+    if (userGuesses[userGuesses.length - 1] !== answers[userGuesses.length - 1]) {
+        title.text(`Game Over: Press Any Key`);
+        gameOver.play();
+        gameStarted = false;
         body.addClass("wrong");
         setTimeout(function () {
             body.removeClass("wrong")
-        }, 31.25);
-    };
-}, 3000);
-
-let btn = $(".btn");
-
-setTimeout(function () {
-    clicked = true;
-
-    if (clicked) {
-        btn.addClass("click");
-        setTimeout(function () {
-            btn.removeClass("click")
-        }, 31.25);
-    };
-}, 6000);
-
-setTimeout(function () {
-    transition = true;
-
-    if (transition) {
-        btn.addClass("transition");
-        setTimeout(function () {
-            btn.removeClass("transition");
-        }, 62.5);
+        }, 131.25);
+    } else {
+        if (userGuesses.length >= answers.length) {
+            level++;
+            title.text(`Level: ${level}`);
+            setTimeout(addAnswer, 650);
+        }
     }
-}, 9000);
+}
+
+function playSound() {
+
+}
